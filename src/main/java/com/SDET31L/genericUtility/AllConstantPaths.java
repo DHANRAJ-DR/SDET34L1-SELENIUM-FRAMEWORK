91d3d2cd180aa4df539121467a1bd3c45b971e10
Dhanraj <Dhanraj@LAPTOP-79C88I1L>
2022-05-27 15:22:03 +0530

    hiiii

--- src/main/java/com/SDET31L/genericUtility/AllConstantPaths.java
+++ src/main/java/com/SDET31L/genericUtility/AllConstantPaths.java
@@ -11,5 +11,10 @@ public interface AllConstantPaths {
 	String EXCELFILEPATH="./src/test/resources/Worksheet.xlsx";
 	String DATABASEURL="jdbc:mysql://localhost:3306/";
     String RMGYANTRA_PROPERTYFILE_PATH="./src/test/resources/rmgyantra.properties";
+ 
+    
+    
+    
+    
     
 }
