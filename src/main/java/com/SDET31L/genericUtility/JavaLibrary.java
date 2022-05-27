2cff2293a6482e01230e87ac3aad9409ac2253e0
Dhanraj <Dhanraj@LAPTOP-79C88I1L>
2022-05-27 17:14:11 +0530

    jl updated

--- src/main/java/com/SDET31L/genericUtility/JavaLibrary.java
+++ src/main/java/com/SDET31L/genericUtility/JavaLibrary.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 /**
  * This class contains only java specific re
  * @author Dhanraj
- * para[agsdhgcjhbdccgcbhgfjhsghxfxjhvckncvhbv 
+ * para[agsdhgcjhbdccgcbhgfjhsghxfxjhvckncvhbv bvn
  */
 public class JavaLibrary {
 	/**
